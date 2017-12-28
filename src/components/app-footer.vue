15b217946b507d8abff409cee31d29371a5b70d2
steven <wangwen1220@139.com>
2017-12-28 18:32:06 +0800

    update

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -2,26 +2,26 @@
   <footer class="app-footer">
     <div id="wx-nav">
       <nav>
-        <router-link to="/" tag="dl" :class="{on: pageName == '微信'}" exact>
+        <dl :class="{on: pageName == '微信'}" @click="slideTo(0)">
           <dt class="iconfont icon-wechat">
             <i v-if="$store.state.newMsgCount" class="new-msg-count">{{$store.state.newMsgCount}}</i>
           </dt>
           <dd>微信</dd>
-        </router-link>
-        <router-link to="#contact" tag="dl" :class="{on: pageName === '通讯录'}">
+        </dl>
+        <dl :class="{on: pageName === '通讯录'}" @click="slideTo(1)">
           <dt class="iconfont icon-contact"></dt>
           <dd>通讯录</dd>
-        </router-link>
-        <router-link to="/find" tag="dl" :class="{on: pageName === '发现'}">
+        </dl>
+        <dl :class="{on: pageName === '发现'}" @click="slideTo(2)">
           <dt class="iconfont icon-find" >
           <i class="new-msg-dot"></i>
           </dt>
           <dd>发现</dd>
-        </router-link>
-        <router-link to="/me" tag="dl" :class="{on: pageName === '我'}">
+        </dl>
+        <dl :class="{on: pageName === '我'}" @click="slideTo(3)">
           <dt class="iconfont icon-me"></dt>
           <dd>我</dd>
-        </router-link>
+        </dl>
       </nav>
     </div>
   </footer>
@@ -33,6 +33,9 @@
       pageName() {
         console.log(this.$store.state.currentPageName)
         return this.$store.state.currentPageName
+      },
+      swiper() {
+        return this.$parent.$refs.vswiper.swiper
       }
     },
     mounted() {
@@ -40,6 +43,11 @@
         if (this.$store.state.msgList.baseMsg[i].read === false && this.$store.state.msgList.baseMsg[i].quiet === false) {
           this.$store.commit('addNewMsg')
         }
+      }
+    },
+    methods: {
+      slideTo(index) {
+        this.swiper.slideTo(index, 300, false)
       }
     }
   }
